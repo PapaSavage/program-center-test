50e2bc9afd8ef6fc98fe520b424983c81abed149
Falaleev Danil <danilkafalaleev@gmail.com>
2024-07-26 21:40:37 +0300

    add pinia, create store

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: "2024-04-03",
+	modules: ["@pinia/nuxt"],
 	css: ["~/assets/css/tailwind.css"],
 	ssr: false,
 	devtools: { enabled: false },
